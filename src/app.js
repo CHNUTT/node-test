ea1e678d5f86206fada07aaebd8ee7634d4d79e0
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-04-14 09:44:52 -0500

    fix bug in app.js assign value to PORT variable from environment

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const { PORT = 4500 } = process.env;
 
 // Load routes
 const routes = require('./apis/routes/index');
 
 app.use('/products', routes.productRoutes);
-
-const PORT = process.env.port || 3000;
 
 app.listen(PORT, () =>
   console.log(`Your application is running on port ${PORT}`)
